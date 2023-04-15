6053b882a5110c8a1a012ffb1c0fd40970ab4345
Ravi Mangal <ravi9788@gmail.com>
2023-04-14 17:50:02 -0700

    print more stats

--- src/fedlab/standalone_pipeline.py
+++ src/fedlab/standalone_pipeline.py
@@ -59,12 +59,14 @@ class EvalPipeline(StandalonePipeline):
             data_loader = self.trainer.dataset.get_dataloader(client, self.trainer.batch_size)
             loss, acc = evaluate(self.handler.model, nn.CrossEntropyLoss(), data_loader)
             # print(f"before personalization: client {client}: "+ "loss {:.4f}, test accuracy {:.4f}".format(loss, acc))
-            label_specific_acc = evaluate_label_specific(self.handler.model, data_loader)
+            label_specific_acc_local = evaluate_label_specific(self.handler.model, data_loader)
+            label_specific_acc_global = evaluate_label_specific(self.handler.model, self.test_loader)
 
             client_stats_pre_personalization[client] = {}
             client_stats_pre_personalization[client]["global_accuracy"] = global_acc
             client_stats_pre_personalization[client]["local_accuracy"] = acc
-            client_stats_pre_personalization[client]["label_specific_local_accuracy"] = label_specific_acc
+            client_stats_pre_personalization[client]["label_specific_local_accuracy"] = label_specific_acc_local
+            client_stats_pre_personalization[client]["label_specific_global_accuracy"] = label_specific_acc_global
             if rules != None:
                 rule_sat_cnt = evaluate_rules(self.handler.model, rules, data_loader)
                 # print(f'before personalization: client {client}: % of inputs satisfying rules {[float(cnt) / len(data_loader.dataset) for cnt in rule_sat_cnt]}')
@@ -83,12 +85,15 @@ class EvalPipeline(StandalonePipeline):
             #      client {client}: "+ "loss {:.4f}, test accuracy {:.4f} (from {:.4f})".format(loss, acc, client_stats_pre_personalization[client]["local_accuracy"]))
             loss_g, acc_g = evaluate(self.trainer._model, nn.CrossEntropyLoss(), self.test_loader)
             # print(f"after personalization: client {client}: "+ "global loss {:.4f}, test accuracy {:.4f} (from {:.4f})".format(loss_g, acc_g, client_stats_pre_personalization[client]["global_accuracy"]))
-            label_specific_acc = evaluate_label_specific(self.trainer._model, data_loader)
+            label_specific_acc_local = evaluate_label_specific(self.trainer._model, data_loader)
+            label_specific_acc_global = evaluate_label_specific(self.trainer._model, self.test_loader)
 
             client_stats_post_personalization[client] = {}
             client_stats_post_personalization[client]["global_accuracy"] = acc_g
             client_stats_post_personalization[client]["local_accuracy"] = acc
-            client_stats_post_personalization[client]["label_specific_local_accuracy"] = label_specific_acc
+            client_stats_post_personalization[client]["label_specific_local_accuracy"] = label_specific_acc_local
+            client_stats_post_personalization[client]["label_specific_global_accuracy"] = label_specific_acc_global
+
             if rules != None:
                 rule_sat_cnt = evaluate_rules(self.trainer._model, rules, data_loader)
                 # print(f'after personalization(# rounds {self.trainer.personalization_rounds}): \
@@ -96,10 +101,10 @@ class EvalPipeline(StandalonePipeline):
                 client_stats_post_personalization[client]["rules"] = [float(cnt) / len(data_loader.dataset) for cnt in rule_sat_cnt]               
             if save: self.save_model(save_path, model = self.trainer._model, name = f"client_{client}")
             
-        print("Before personalization results:")
+        print("\nBefore personalization results:")
         for id, client in enumerate(clients):
             print(f'Client {client}: ', client_stats_pre_personalization[client])
-        print("After personalization results:")
+        print("\nAfter personalization results:")
         for id, client in enumerate(clients):
             print(f'Client {client}: ', client_stats_post_personalization[client])
 
