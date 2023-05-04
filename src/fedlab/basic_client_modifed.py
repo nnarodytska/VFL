386c63333b1b1a7093c3beb58a1f196cace463ad
Ravi Mangal <ravi9788@gmail.com>
2023-05-04 13:43:24 -0700

    added repair function for each client

--- src/fedlab/basic_client_modifed.py
+++ src/fedlab/basic_client_modifed.py
@@ -1,5 +1,5 @@
 from fedlab.contrib.algorithm.basic_client import SGDSerialClientTrainer
-from utils import map_inputs_to_rules, calculate_dist_to_rule, calculate_similarity_loss
+from utils import map_inputs_to_rules, calculate_dist_to_rule, calculate_similarity_loss, evaluate_rules
 
 import torch.nn as nn
 import torch
@@ -43,6 +43,25 @@ class SGDSerialClientTrainerExt(SGDSerialClientTrainer):
         """
         self.sim_weight = weight
 
+    def local_process(self, payload, id_list):
+        model_parameters = payload[0]
+        for id in id_list:
+            data_loader = self.dataset.get_dataloader(id, self.batch_size)
+
+            self.set_model(model_parameters)
+            # map from data points to rules they satisfy
+            if not (self.rules is None):
+                input_to_rule_map = map_inputs_to_rules(self.model, self.rules, data_loader)
+            pack = self.train(model_parameters, data_loader)
+
+            if self.rules != None:
+                rule_sat_cnt = evaluate_rules(self.model, self.rules, data_loader)
+                print(f'before repair: \
+                      client {id}: % of inputs satisfying rules {[float(cnt) / len(data_loader.dataset) for cnt in rule_sat_cnt]}')
+
+            pack = self.repair(model_parameters, data_loader, input_to_rule_map)
+            self.cache.append(pack)
+
     def train(self, model_parameters, train_loader):
         """Single round of local training for one client.
 
@@ -85,57 +104,11 @@ class SGDSerialClientTrainerExt(SGDSerialClientTrainer):
                         return [self.model_parameters]
                     else:
                         output = self.model(data)
-                        loss_base = self.criterion(output, target)
+                        loss = self.criterion(output, target)
 
-                        if self.rules == None:
-                            loss = loss_base
-                        else:
-                            # calculate "distance" of internal representation from rules and corresponding loss
-                            # latent_vectors = self.model.input_to_representation(data)
-
-                            #         dist_rep_to_rule = calculate_dist_to_rule(input_to_rule_map[idx_strt:idx_strt+batch_size], latent_vectors, self.rules)
-                            #         loss_sim = calculate_similarity_loss(dist_rep_to_rule)
-
-                            #         loss = loss_base + self.sim_weight * loss_sim
-                            #     self.optimizer.zero_grad()
-                            #     loss.backward()
-                            #     self.optimizer.step()
-
-                            global_w = self.model.fc2.weight.detach().cpu().numpy()
-                            global_b = self.model.fc2.bias.detach().cpu().numpy()
-
-                            gurobi_model = gp.Model("personalization")
-                            nrow, ncol = global_w.shape
-                            global_w = global_w.flatten()
-                            indices = [*range(nrow * ncol)]
-                            gurobi_w = gurobi_model.addVars(indices, lb=-GRB.INFINITY, vtype=GRB.CONTINUOUS, name="w")
-                            gurobi_model.setObjective(gp.quicksum((gurobi_w[i] - global_w[i]) ** 2 for i in indices),
-                                                      GRB.MINIMIZE)
-
-                            layer2_input = self.model.layer2_input(data).detach().cpu().numpy()
-                            for i in range(len(data)):
-                                rule = self.rules[input_to_rule_map[i]]
-                                for id in range(len(rule[1])):
-                                    constraint = gp.quicksum(
-                                        layer2_input[i][j] * gurobi_w[j * ncol + id] + global_b[id] for j in range(ncol))
-                                    op = rule[2][2 * id]
-                                    vsig = rule[2][2 * id + 1]
-
-                                    if op == "<=":
-                                        gurobi_model.addConstr(constraint <= vsig)
-                                    else:
-                                        # gurobi hard to optimize >
-                                        gurobi_model.addConstr(constraint >= vsig)
-
-                            gurobi_model.feasRelaxS(1, True, False, True)
-                            gurobi_model.optimize()
-
-                            optimized_w = torch.tensor([gurobi_w[i].x for i in indices]).reshape(nrow, ncol)
-
-                            if self.cuda:
-                                optimized_w = optimized_w.cuda(self.device)
-
-                            self.model.fc2.weight = nn.Parameter(optimized_w)
+                        self.optimizer.zero_grad()
+                        loss.backward()
+                        self.optimizer.step()
 
                 else:
                     output = self.model(data)
@@ -148,5 +121,70 @@ class SGDSerialClientTrainerExt(SGDSerialClientTrainer):
                 rounds += 1
                 idx_strt += batch_size
                         
+
+        return [self.model_parameters]
+    
+
+    def repair(self, model_parameters, train_loader, input_to_rule_map):
+        """Repair weights for one client using Gurobi.
+
+        Args:
+            model_parameters (torch.Tensor): serialized model parameters.
+            train_loader (torch.utils.data.DataLoader): :class:`torch.utils.data.DataLoader` for this client.
+        """
+        # print(self._model)
+        # print(model_parameters.shape)
+        self.set_model(model_parameters)
+        self._model.train()
+
+        rounds = 0
+        #print("----")
+        #print(self.model_parameters)
+        #print(self.optimizer)
+
+        if self.rules == None:
+            return [self.model_parameters]
+        
+        for data, target in train_loader:
+            if self.cuda:
+                data = data.cuda(self.device)
+                target = target.cuda(self.device)
+
+            if (self.personalization):
+                global_w = self.model.fc2.weight.detach().cpu().numpy()
+                global_b = self.model.fc2.bias.detach().cpu().numpy()
+
+                gurobi_model = gp.Model("personalization")
+                nrow, ncol = global_w.shape
+                global_w = global_w.flatten()
+                indices = [*range(nrow * ncol)]
+                gurobi_w = gurobi_model.addVars(indices, lb=-GRB.INFINITY, vtype=GRB.CONTINUOUS, name="w")
+                gurobi_model.setObjective(gp.quicksum((gurobi_w[i] - global_w[i]) ** 2 for i in indices),
+                                            GRB.MINIMIZE)
+
+                layer2_input = self.model.layer2_input(data).detach().cpu().numpy()
+                for i in range(len(data)):
+                    rule = self.rules[input_to_rule_map[i]]
+                    for id in range(len(rule[1])):
+                        constraint = gp.quicksum(
+                            layer2_input[i][j] * gurobi_w[j * ncol + id] + global_b[id] for j in range(ncol))
+                        op = rule[2][2 * id]
+                        vsig = rule[2][2 * id + 1]
+
+                        if op == "<=":
+                            gurobi_model.addConstr(constraint <= vsig)
+                        else:
+                            # gurobi hard to optimize >
+                            gurobi_model.addConstr(constraint >= vsig)
+
+                gurobi_model.feasRelaxS(1, True, False, True)
+                gurobi_model.optimize()
+
+                optimized_w = torch.tensor([gurobi_w[i].x for i in indices]).reshape(nrow, ncol)
+
+                if self.cuda:
+                    optimized_w = optimized_w.cuda(self.device)
+
+                self.model.fc2.weight = nn.Parameter(optimized_w)
 
         return [self.model_parameters]
