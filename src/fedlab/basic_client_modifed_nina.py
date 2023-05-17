2e860de72f1c89f1c544b9954ea6fdd6824b07ae
Nina <n.narodytska@gmail.com>
2023-05-17 09:18:28 -0700

    temp comimt

---
+++ src/fedlab/basic_client_modifed_nina.py
@@ -0,0 +1,230 @@
+from fedlab.contrib.algorithm.basic_client import SGDSerialClientTrainer
+from utils import map_inputs_to_rules, calculate_dist_to_rule, calculate_similarity_loss
+
+import torch.nn as nn
+import torch
+
+class SGDSerialClientTrainerExt(SGDSerialClientTrainer):
+    def __init__(self, model, num_clients, cuda=False, device=None, logger=None, personal=False, 
+                 personalization = False, personalization_rounds = -1, rules = None, sim_weight = 1, 
+                 concept_representation = None, global_model = None) -> None:
+        super().__init__(model, num_clients, cuda, device, personal)
+        self.personalization_rounds = personalization_rounds
+        self.personalization = personalization
+        self.rules = rules
+        self.sim_weight = sim_weight
+        self.concept_representation = concept_representation
+        self.global_model = global_model
+
+    def ok(self):
+        print("ok")
+
+    def setup_lr(self,  lr):
+        """Set up local optimization configuration.
+
+        Args:
+            lr (float): Learning rate.
+        """
+        self.optimizer = torch.optim.SGD(self._model.parameters(), lr)
+
+    def setup_rules(self, rules):
+        """Set up rules that are to be maintained.
+
+        Args:
+            rules (List): List of rules where each rule is a triple of the form (class, neuron ids, neuron signature).
+        """
+        self.rules = rules
+
+    def setup_sim_weight(self, weight):
+        """Set up sim_weight.
+
+        Args:
+            weight (float): sim_weight.
+        """
+        self.sim_weight = weight
+    
+    def setup_global_model(self, global_model):
+        """Set up global_model.
+
+        Args:
+            global_model (torch.nn.Module): global_model.
+        """
+        self.global_model = global_model
+
+    def setup_optim(self, epochs, batch_size, lr):
+        """Set up local optimization configuration.
+
+        Args:
+            epochs (int): Local epochs.
+            batch_size (int): Local batch size. 
+            lr (float): Learning rate.
+        """
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.lr = lr
+        params = []
+        if self.concept_representation == "linear" or  self.concept_representation  is None:
+            layer_names = []
+            for name, module in self._model.named_modules():
+                if module in self._model.pred_layers:
+                    layer_names.append(name)
+        
+            for name, param in self._model.named_parameters():
+                unfrozen_param_names = [f"{layer}.weight" for layer in layer_names] + [f"{layer}.bias" for layer in layer_names]
+                if name not in unfrozen_param_names:
+                    param.requires_grad = False
+                else:
+                    param.requires_grad = True
+
+            self.optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, self._model.parameters()), lr)
+            # for layer in self._model.pred_layers:
+            #     params += list(layer.parameters())
+        else:
+            params = self._model.parameters()
+            self.optimizer = torch.optim.SGD(params, lr)
+
+        self.criterion = torch.nn.CrossEntropyLoss()
+
+    def train(self, model_parameters, train_loader):
+        """Single round of local training for one client.
+
+        Note:
+            Overwrite this method to customize the PyTorch training pipeline.
+
+        Args:
+            model_parameters (torch.Tensor): serialized model parameters.
+            train_loader (torch.utils.data.DataLoader): :class:`torch.utils.data.DataLoader` for this client.
+        """
+        if self.personalization and self.concept_representation == "decision_tree" and self.rules == None:
+            raise Exception("If using decision tree rules for personalization, rules need to be provided.")
+        
+        if self.personalization and self.concept_representation == "linear" and not hasattr(self._model, "concept_layers"):
+            raise Exception("If using linear concepts for personalization, model needs to have concept layers.")
+        
+
+        # print(self._model)
+        # print(model_parameters.shape)
+        self.set_model(model_parameters)
+
+        rounds = 0
+        #print("----")
+        #print(self.model_parameters)
+        #print(self.optimizer)
+
+        # map from data points to rules they satisfy
+        # if not (self.rules is None):
+        if self.personalization and self.concept_representation == "decision_tree":
+            input_to_rule_map = map_inputs_to_rules(self.global_model, self.rules, train_loader)
+        
+        if self.personalization and self.concept_representation == "linear":
+            self.global_model.eval()
+            self.global_model.start_probe_mode()
+            input_to_concept_labels = []
+            with torch.no_grad():
+                cnt = 0
+                for data, target in train_loader:
+                    if self.cuda:
+                        data = data.cuda(self.device)
+                        target = target.cuda(self.device)
+                    concept_outputs = self.global_model(data)[1:-1]
+                    concept_labels = []
+                    for c_idx, concept_output in enumerate(concept_outputs):
+                        concept_labels.append(torch.unsqueeze(torch.argmax(concept_output, dim=1), dim=-1))
+                        if cnt == 0:
+                            print(f"init c_idx {c_idx}")
+                            print(f"init concept_output {concept_output}")
+
+                    input_to_concept_labels.append(torch.cat(concept_labels, dim=1))
+                  
+                    cnt = cnt + 1
+                input_to_concept_labels = torch.cat(input_to_concept_labels)
+            self.global_model.stop_probe_mode()
+                
+        self._model.train()
+        batch_size = train_loader.batch_size
+        # print(self.epochs)
+        # exit()
+        for epoch in range(self.epochs):
+            idx_strt = 0
+            for data, target in train_loader:
+                if self.cuda:
+                    data = data.cuda(self.device)
+                    target = target.cuda(self.device)
+                # if epoch == 0:
+                #     print(data)
+                #     print(target)
+                    
+
+                if (self.personalization):
+                    # for name, module in self._model.named_modules():
+                    #     #print( name, module)    
+                    #     if name in ["fc1", "fc2"]:
+                    #         print(f"rounds {rounds}: {name} {module.bias}")
+                                  
+                    #print(self.concept_representation)                
+                    assert(self.personalization_rounds != -1)
+                    # print(f"personalization_rounds = {rounds}")
+                    if rounds >= self.personalization_rounds:
+                        #print(self.model_parameters)
+                        #exit()
+                        return [self.model_parameters]
+                    else:
+                        if self.concept_representation == None:
+                            output = self.model(data)
+                            loss_base = self.criterion(output, target)
+                            loss = loss_base
+                            self.optimizer.zero_grad()
+                            loss.backward()
+                            self.optimizer.step()
+
+                        elif self.concept_representation == "decision_tree":
+                            output = self.model(data)
+                            loss_base = self.criterion(output, target)
+                            # calculate "distance" of internal representation from rules and corresponding loss
+                            latent_vectors = self.model.input_to_representation(data)
+                            dist_rep_to_rule = calculate_dist_to_rule(input_to_rule_map[idx_strt:idx_strt+batch_size], latent_vectors, self.rules)
+                            loss_sim = calculate_similarity_loss(dist_rep_to_rule)
+                            loss = loss_base + self.sim_weight * loss_sim
+                            self.optimizer.zero_grad()
+                            loss.backward()
+                            self.optimizer.step()
+                            #print(len(target[target == 0]), len(target[target == 1]), float(loss))
+
+                        elif self.concept_representation == "linear":
+                            self._model.start_probe_mode()
+                            output = self.model(data)
+
+
+                            loss_base = self.criterion(output[0], target)
+                            concept_labels = output[1:-1]
+                            loss_concept = 0
+                            for c_idx, concept_label in enumerate(concept_labels):
+                                loss_concept += self.criterion(concept_label, input_to_concept_labels[idx_strt:idx_strt+batch_size, c_idx])
+                                if rounds <= 1:
+                                    print(f"rounds {rounds} c_idx {c_idx} loss {self.criterion(concept_label, input_to_concept_labels[idx_strt:idx_strt+batch_size, c_idx])}")
+                                    print(concept_label) 
+                                    print(input_to_concept_labels[idx_strt:idx_strt+batch_size, c_idx])
+
+                            loss = loss_base + self.sim_weight * loss_concept
+                            #print(loss_base, self.sim_weight, loss_concept, self.sim_weight * loss_concept)
+                            self.optimizer.zero_grad()
+                            loss.backward()
+                            self.optimizer.step()
+                            self._model.stop_probe_mode()
+                            if rounds == 2:
+                                exit()
+
+             
+
+                else:
+                    output = self.model(data)
+                    loss = self.criterion(output, target)
+
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
+
+                rounds += 1
+                idx_strt += batch_size
+                        
+        return [self.model_parameters]
